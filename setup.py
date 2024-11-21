ab23628ac0ec020e4b77fe540fbfcb69c4a50df3
bqio <dmitry.mikhaltsov98@gmail.com>
2024-11-21 09:43:35 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="libcli",
     version="1.0",
     packages=find_packages(),
-    entry_points={"console_scripts": ["libcli=libcli.__main__"]},
+    entry_points={"console_scripts": ["libcli=libcli:__main__"]},
 )
